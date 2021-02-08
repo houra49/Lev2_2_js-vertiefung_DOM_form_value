b56cd3d977fc722c150eb5a373cbc080dbdb0650
houra49 <houra.shamshyrcar@gmail.com>
2021-02-08 14:12:38 +0100

    Lev2_2_js-vertiefung_DOM_form_value

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const vorname = document.getElementById("vorname")
+const nachname = document.getElementById("nachname")
+const land = document.getElementById("land")
+function zeigen() {
+    console.log("full name :" + " " + vorname.value + " " + nachname.value + " " + "from" + " " + land.value)
+}
